b180efc31ebc58e6a041025ec129a41abea294d0
Teslenko A.A.(work) <alteslo31@gmail.com>
2021-12-14 17:59:07 +0300

    add text

--- tgbot/handlers/stat_data_proc.py
+++ tgbot/handlers/stat_data_proc.py
@@ -47,10 +47,17 @@ async def stat_have_datas(call: types.CallbackQuery,
 
 async def stat_havent_datas(call: types.CallbackQuery,
                             state: FSMContext):
-    keyboard = await inline.kb_yes_no()
+    # keyboard = await inline.kb_yes_no()
     await state.update_data(have_data="Нет")
     print(await state.get_data())
-    await call.message.edit_text(text="Кракозябра")
+    text = (
+        """Для статистической обработки необходимо собрать данные. Для того чтобы
+    Вам было удобнее мы подготовили для Вас шаблон базы данных и памятку по данным.
+    Так Вы сможете формировать данные сразу в необходимом формате для
+    статистической обработки. Скачать их можно ниже"""
+    )
+
+    await call.message.edit_text(text=text)
     await call.message.answer("https://docs.google.com/spreadsheets/d/1xg4KaPw-amRCGW5RYAqKmU26Z5R6THVM/edit?usp=sharing&ouid=114362960612507406007&rtpof=true&sd=true")
 
     await Interview.waiting_for_stat_types_yes_no_answer.set()
